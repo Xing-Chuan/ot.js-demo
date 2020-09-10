6509027efa8d545ddcd8115dcaa1c5e71079f1ce
wanggm <wanggm@liepin.com>
2020-09-10 17:50:10 +0800

    chore: refresh

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-
-import Coding from '../components/Coding';
-
-// ReactDom.render(<Coding/>, document.getElementById('editor'))
-ReactDom.render(<div>kkk</div>, document.getElementById('editor'))
